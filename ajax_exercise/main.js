969a37029509fb9d7f66b74bf043bd7806379a7f
Maria Cristina Del Valle <32019417+mardelvalle@users.noreply.github.com>
2017-12-04 09:48:24 -0500

    Added error messages

--- ajax_exercise/main.js
+++ ajax_exercise/main.js
@@ -1,3 +1,8 @@
+$("#getWord").keyup(function(event) {
+  if (event.keyCode === 13) {
+    $("#enter").click();
+  }
+})
 $("#enter").on("click",function(){
 
   var apiKey = "";
@@ -9,37 +14,19 @@ $("#enter").on("click",function(){
     // putting together the url
     console.log(t);
     console.log(t.records[0])
+    if(word==null||word==""){
+      alert("please enter a word in the field")
+    }
+    else if(t.records[0]==null || t.records[0]==""){
+      alert("Sorry there is nothing that matches your search. Please try again")
+      }
+    else{
     $("img").attr( "src", t.records[0].primaryimageurl)
     // pulling the image from the first
     $("h2").text(t.records[0].named)
     // pulling the name
     $("h3").text(t.records[0].dated)
     $("h4").text(t.records[0].url)
+}
 })
 })
-
-    // placing the temperature and doing the appropriate math
-
-// $.get(apiURL,function(r){
-//   console.log(apiURL);
-// })
-// $.ajax({
-//   url: apiURL,
-//   success: function(result){
-//     $("div").append( "<img src=" + result.url +">")
-//
-// })
-// ajax takes an object as their part ($)
-//   $.ajax({
-//     url: apiURL,
-//     success: function(result){
-//       $("div").append( "<img src=" + result.url +">")
-//       //data is retrieved successfully
-//       // you can change the line to be more like $("img").attr("src".result.hurl)
-//       // date picker could also allow to have multiple fields for the imput
-//     },
-//     error:function(r){
-//       $("div").append("please enter a valid date")
-//     }
-//   });
-// })
